c8819a09ce2ff92bbcaa1088517b3dfbdc39cc2a
Chloe <chloelo@open-life.com>
2020-11-23 18:19:52 +0800

    完成 admin 頁

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,12 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
+$(function () {
+  // 原路徑會有 /， eg: /admin.html，要把斜線去掉
+  var current = location.pathname.substring(1);
+  $('.sidebar-nav .link').each(function () {
+    var $this = $(this);
+    if ($this.attr('href').indexOf(current) !== -1) {
+      $this.addClass('active');
+    }else{
+      $this.removeClass('active');
+    }
+  })
+})
