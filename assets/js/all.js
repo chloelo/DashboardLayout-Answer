7c75d997fb18a6bb8a8d416e07df3a327e3c7102
Chloe <chloelo@open-life.com>
2020-11-23 18:21:00 +0800

    Update 2020-11-23T10:20:55.739Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,16 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+$(function () {
+  // 原路徑會有 /， eg: /admin.html，要把斜線去掉
+  var current = location.pathname.substring(1);
+  $('.sidebar-nav .link').each(function () {
+    var $this = $(this);
+
+    if ($this.attr('href').indexOf(current) !== -1) {
+      $this.addClass('active');
+    } else {
+      $this.removeClass('active');
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
