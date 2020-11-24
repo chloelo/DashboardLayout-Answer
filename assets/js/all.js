ababaece0c11dbafc3abc9e0bd66bd62b6fee2bf
Chloe <chloelo@open-life.com>
2020-11-24 18:06:31 +0800

    Update 2020-11-24T10:06:27.242Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -8,7 +8,7 @@ $(function () {
     // 第一個斜線後方是資料夾名稱，第二個斜線之後才是路徑名稱，所以用 split 這個字串切割的方法
     // 取得用斜線來分割的第二個字串 (也就是當下路徑) 
 
-    if ($this.attr('href').indexOf(current.split('/')[1]) !== -1) {
+    if ($this.attr('href').indexOf(current.split('/')[2]) !== -1) {
       $this.addClass('active');
     }
   });
